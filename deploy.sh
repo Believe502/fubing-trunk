9b245586c14845e73f06e17bb7742cd3c796acfd
wuqiwei <1058633117@qq.com>
2015-10-15 23:55:17 +0800

    修改部署文件

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+kill -9 $(echo $(ps -ef | grep tomcat) | cut -d " " -f 2)
+git pull
 mvn clean compile war:war
 mv target/*.war target/ROOT.war
+rm -rf /usr/local/tomcat6/webapps/ROOT.*
+mv target/ROOT.war /usr/local/tomcat6/webapps/
+/usr/local/tomcat6/bin/startup.sh
 tail -n 200 -f /usr/local/tomcat6/logs/catalina.out
